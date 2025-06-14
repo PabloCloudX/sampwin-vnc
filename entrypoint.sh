0a1023b31771963c390a53bd8ba445e1db3a02f7
PabloCloudX <lilikfarida067@gmail.com>
2025-06-14 19:09:53 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+supervisord &
+
+sleep 2
+
+if [ "$TYPE" == "server" ]; then
+    echo "[INFO] Menjalankan SA-MP Server..."
+    wine samp-server.exe
+elif [ "$TYPE" == "pawno" ]; then
+    echo "[INFO] Menjalankan Pawno Editor..."
+    x11vnc -display :0 -rfbport "$VNC_PORT" -usepw -forever &
+    wine pawno.exe
+else
+    echo "[INFO] Tidak ada TYPE valid. Gunakan 'server' atau 'pawno'."
+    sleep infinity
+fi
