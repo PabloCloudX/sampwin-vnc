0087345b35c30cb1c9a8d12bc59b9303e744a043
PabloCloudX <lilikfarida067@gmail.com>
2025-06-14 20:41:01 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 export HOME=/home/container
+export DISPLAY=:1
 
-supervisord &
+# Buat password VNC
+mkdir -p /home/container/.vnc
+x11vnc -storepasswd 1235678 /home/container/.vnc/passwd
+
+# Jalankan Xvfb (virtual display)
+Xvfb :1 -screen 0 1024x768x16 &
+
+# Jalankan VNC server
+x11vnc -display :1 -rfbauth /home/container/.vnc/passwd -rfbport "$VNC_PORT" -forever &
 
 sleep 2
 
-if [ "$TYPE" == "server" ]; then
-    echo "[INFO] Menjalankan SA-MP Server..."
-    wine /home/container/samp-server.exe
-
-elif [ "$TYPE" == "pawno" ]; then
-    echo "[INFO] Menjalankan Pawno Editor..."
-    
-    mkdir -p /home/container/.vnc
-    x11vnc -storepasswd 1235678 /home/container/.vnc/passwd
-
-    x11vnc -display :0 -rfbauth /home/container/.vnc/passwd -rfbport "$VNC_PORT" -forever &
-
+if [ "$TYPE" = "pawno" ]; then
+    echo "[INFO] Menjalankan Pawno Editor"
     wine /home/container/pawno/pawno.exe
-
+elif [ "$TYPE" = "server" ]; then
+    echo "[INFO] Menjalankan SA-MP Server"
+    wine /home/container/samp-server.exe
 else
-    echo "[INFO] TYPE tidak valid. Gunakan 'server' atau 'pawno'."
+    echo "[ERROR] TYPE tidak valid: gunakan 'pawno' atau 'server'"
     sleep infinity
 fi
